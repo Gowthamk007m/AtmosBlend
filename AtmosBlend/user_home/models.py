ed97e5f737dfbb234c4c54d4eeaf62b6d8eb47d6
Gowtham K M <116723241+Gowthamk007m@users.noreply.github.com>
2024-04-04 12:51:59 +0530

    1

--- AtmosBlend/user_home/models.py
+++ AtmosBlend/user_home/models.py
@@ -24,6 +24,7 @@ class CustomUserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
+
     def create_user(self, phone,username, email, password, **extra_fields):
         extra_fields.setdefault('is_staff', False)
         extra_fields.setdefault('is_superuser', False)
